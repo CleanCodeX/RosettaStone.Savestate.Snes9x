3216d22bf0a61063c295aa3af6c9d7ad9bcd2035
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2021-01-09 02:36:27 +0100

    added Load optimization

--- Helpers/SavestateManager.cs
+++ Helpers/SavestateManager.cs
@@ -5,16 +5,23 @@ using SavestateFormat.Snes9x.Models.Structs;
 
 namespace SavestateFormat.Snes9x.Helpers
 {
+	public enum SavestateLoadMode
+	{
+		All,
+		SRAM
+	}
+
 	public class SavestateManager
 	{
-		public Savestate Load(string filepath)
+		public Savestate Load(in string filepath) => Load(filepath, SavestateLoadMode.SRAM);
+		public Savestate Load(in string filepath, in SavestateLoadMode mode)
 		{
 			using var file = new FileStream(filepath, FileMode.Open, FileAccess.Read);
 			Savestate result = default;
 
 			if (file.CanRead)
 			{
-				result = Load(file);
+				result = Load(file, mode);
 
 				file.Close();
 			}
@@ -24,7 +31,8 @@ namespace SavestateFormat.Snes9x.Helpers
 			return result;
 		}
 
-		public Savestate Load(Stream stream)
+		public Savestate Load(in Stream stream) => Load(stream, SavestateLoadMode.SRAM);
+		public Savestate Load(in Stream stream, in SavestateLoadMode mode)
 		{
 			var uncompressed = GzipHelper.Decompress(stream);
 			var ms = new MemoryStream(uncompressed);
@@ -46,17 +54,21 @@ namespace SavestateFormat.Snes9x.Helpers
 				DMA = ms.ReadFileBlock(),
 				VRA = ms.ReadFileBlock(),
 				RAM = ms.ReadFileBlock(),
-				SRA = ms.ReadFileBlock(), // SRAM
-				FIL = ms.ReadFileBlock(), 
-				APU = ms.ReadFileBlock(), 
-				ARE = ms.ReadFileBlock(), // (if emulating the APU)
-				ARA = ms.ReadFileBlock(), // (if emulating the APU)
-				SOU = ms.ReadFileBlock(), // (if emulating the APU)
-				SA1 = ms.ReadFileBlock(), // (if emulating the SA-1)
-				SAR = ms.ReadFileBlock(), // (if emulating the SA-1)
-				SP7 = ms.ReadFileBlock(), // (if emulating the SPC7110)
-				RTC = ms.ReadFileBlock() // (if emulating the SPC7110 RTC)
+				SRA = ms.ReadFileBlock() // SRAM
 			};
+
+			if(mode == SavestateLoadMode.SRAM)
+				return result;
+
+			result.FIL = ms.ReadFileBlock();
+			result.APU = ms.ReadFileBlock();
+			result.ARE = ms.ReadFileBlock(); // (if emulating the APU)
+			result.ARA = ms.ReadFileBlock(); // (if emulating the APU)
+			result.SOU = ms.ReadFileBlock(); // (if emulating the APU)
+			result.SA1 = ms.ReadFileBlock(); // (if emulating the SA-1)
+			result.SAR = ms.ReadFileBlock(); // (if emulating the SA-1)
+			result.SP7 = ms.ReadFileBlock(); // (if emulating the SPC7110)
+			result.RTC = ms.ReadFileBlock(); // (if emulating the SPC7110 RTC)
 
 			return result;
 		}
